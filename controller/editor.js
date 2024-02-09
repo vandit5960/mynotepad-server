993065f2445e68203cda5ac0c538336238df91db
Vandit Mehta <108853368+vandit5960@users.noreply.github.com>
2024-02-09 17:40:26 +0530

    Update editor.js

--- controller/editor.js
+++ controller/editor.js
@@ -54,6 +54,26 @@ const setPassword = async (req, res) => {
 };
 
 const setUsername = async (req, res) => {
+  try {
+    const { name, username } = req.body;
+    const editor = await Editor.findOne({ name: username });
+    const isAvailable = !!editor;
+    const same = name === username;
+    if (isAvailable && !same) {
+      return res.status(200).json({ error: "Already Existed" });
+    } else {
+      const updatedData = await Editor.findOneAndUpdate(
+        { name: name },
+        { name: username },
+        { new: true }
+      );
+      return res.status(200).json({ updatedData });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
 
 module.exports = { getData, putData, setPassword,setUsername };
